68580ea231e15202acea0da865dcf58757897d37
Walter Caraza Barrera <waltercaraza@gmail.com>
2013-07-25 22:52:30 -0500

    Create rest-server.php

---
+++ rest-server.php
@@ -0,0 +1,123 @@
+<?php 
+
+class ServicesWeb
+{
+/**
+* Write to a file
+*
+* @param string $string
+* @return string Some return message
+*/ 
+
+    
+  public function iorder($appid)
+  {
+      /*$this->_itemModelo = new Models_ItemModelo;
+      $itemArray=$this->_itemModelo->listItemAll($appid);
+
+      foreach($itemArray as $key => $value){
+          $itemData[] = $value;
+      }
+      $response["items"] = $itemData;*/
+      
+      $response["producto"] = array(array("id" => 7, "nombre" => "Chicharron de pollo", "descripcion" => "","stock" => 30, "precio" => 10.5, "idestado"=> 1, "imagen" => "http://a3.twimg.com/profile_images/740897825/AndroidCast-350_normal.png"));
+      $response["cliente"] = array(array("id" => 8, "nombre" => "Mesa 8"));
+      $response["producto_galeria"] = array(array("id" => 1, "idproducto" => 1, "imagen" => "http://farm6.static.flickr.com/5186/5841048399_8d9d7aeefe.jpg", "file" => "5841048399_8d9d7aeefe.jpg", "idestado" => 1),
+          array("id" => 2, "idproducto" => 1, "imagen" => "http://www.generaccion.com/w/imagenes/gastronomia/recetas/frijoles-con-seco.jpg", "file" => "frijoles-con-seco.jpg", "idestado" => 1),
+          array("id" => 3, "idproducto" => 7, "imagen" => "http://7artminds.com/nikiweb/Imagenes/chicharron-de-pollo.jpg", "file" => "chicharron-de-pollo.jpg", "idestado" => 1));
+      
+      exit(Zend_Json::encode($response));
+  }
+  
+  public function token($hash,$idapp,$lang=null)
+  {
+      $this->_pushModelo = new Models_PushModelo;
+      $insertData = array('token' => $hash, 'id_app' => $idapp, 'lang' => $lang);
+$this->_pushModelo->insert($insertData);
+      
+      $response = array('success'=>1);
+      exit(Zend_Json::encode($response));
+  }
+  
+  public function user_registration()
+  {
+      $this->_userModelo = new Models_UserModelo;
+$this->_userModelo->insert($_POST);
+      
+      $response = array('success'=>1);
+      exit(Zend_Json::encode($response));
+  }
+}
+
+class RestServerController extends Zend_Rest_Controller
+{
+  /*public function  preDispatch()
+  {
+      
+  }
+
+public function indexAction()
+{
+          for($i=0;$i<=10;$i++){
+          $x[$i] = array("firstName" =>"test1","lastName" =>"test2","email" =>"test3");
+          }
+          exit(json_encode($x));
+	
+  }*/
+  
+  public function init()
+  {
+      $this->_helper->viewRenderer->setNoRender(true);
+      
+      $this->_todo = array (
+      "1" => "Buy milk",
+      "2" => "Pour glass of milk",
+      "3" => "Eat cookies"
+      );
+  }
+  
+  
+  public function restAction()
+  {
+      $server = new Zend_Rest_Server();
+      $server->setClass("ServicesWeb");
+      $server->handle();
+      exit;
+  }
+
+  public function indexAction()
+  {
+       
+      /*$this->getResponse()
+          ->appendBody("From indexAction() returning all articles");*/
+      echo Zend_Json::encode($this->_todo);
+  }
+
+  public function getAction()
+  {
+      $this->getResponse()
+          ->appendBody("From getAction() returning the requested article");
+
+  }
+  
+  public function postAction()
+  {
+      $this->getResponse()
+          ->appendBody("From postAction() creating the requested article");
+
+  }
+  
+  public function putAction()
+  {
+      $this->getResponse()
+          ->appendBody("From putAction() updating the requested article");
+
+  }
+  
+  public function deleteAction()
+  {
+      $this->getResponse()
+          ->appendBody("From deleteAction() deleting the requested article");
+
+  }
+}
